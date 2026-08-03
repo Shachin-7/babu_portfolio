cb04f4e4d5e3b15576bbadb642040b96532bfd56
Shachin-7 <shachinvp0506@gmail.com>
2026-08-03 19:04:58 +0530

    feat: remove About Me button from hero section CTAs

--- index.js
+++ index.js
@@ -2559,7 +2559,7 @@ function render(){
     if (hhTitle) hhTitle.style.opacity = String(1 - smooth(p/.3));
     attrs.style.opacity    = String(1 - smooth(p/.4));
     cornerL.style.opacity  = String(1 - smooth(p/.35));
-    btnAbout.style.opacity = String(1 - smooth(p/.4));
+    if (btnAbout) btnAbout.style.opacity = String(1 - smooth(p/.4));
     giant.style.opacity    = String(1 - fadeHero);
 
     /* Sidebar container fade + slide in */
